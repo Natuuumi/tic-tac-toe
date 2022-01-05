323828f31652d9023c48ad3f7da0bbca9932d509
Nat Macbook <rubix153@gmail.com>
2022-01-04 23:26:11 -0500

    updated js

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
 const ticTacToe = (function() {
 	let boxSelection = document.querySelectorAll('.gameSquare');
 	let boxArray = [ ...boxSelection ];
-	console.log(boxArray);
 
 	gameBoard = {
 		currentPlayer: 'X',
 		board: [ '', '', '', '', '', '', '', '', '' ]
 	};
+
 	function renderGame() {
 		boxArray.forEach((e) => {
 			e.textContent = gameBoard.board[boxArray.indexOf(e)];
 		});
 	}
-	function selection() {
-		boxArray.forEach((element) => {
-			element.addEventListener('click', () => {
-				if (gameBoard.board[boxArray.indexOf(element)] === '') {
-					gameBoard.board[boxArray.indexOf(element)] = gameBoard.currentPlayer;
-					renderGame();
-					if (gameBoard.currentPlayer === 'X') {
-						gameBoard.currentPlayer = 'O';
-					} else {
-						gameBoard.currentPlayer = 'X';
-					}
-				}
-			});
-		});
+	function checkWinner() {
+		if (gameBoard.board[(1, 2, 3)]) {
+		}
 	}
-	selection();
-
 	return {
-		render: renderGame
+		Play() {
+			boxArray.forEach((element) => {
+				element.addEventListener('click', () => {
+					if (gameBoard.board[boxArray.indexOf(element)] === '') {
+						gameBoard.board[boxArray.indexOf(element)] = gameBoard.currentPlayer;
+						renderGame();
+						if (gameBoard.currentPlayer === 'X') {
+							gameBoard.currentPlayer = 'O';
+						} else {
+							gameBoard.currentPlayer = 'X';
+						}
+					}
+				});
+			});
+		}
 	};
 })();
-ticTacToe.render();
+ticTacToe.Play();
