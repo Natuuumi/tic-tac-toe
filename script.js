45b07e49b314606e7cffcb93da207a19b9be455c
Natuuumi <56280643+Natuuumi@users.noreply.github.com>
2022-01-05 04:44:38 -0500

    finished tic tac toe

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
 const ticTacToe = (function () {
-  let gameText = document.querySelector("#gameText");
-  let boxSelection = document.querySelectorAll(".gameSquare");
+  const gameText = document.querySelector("#gameText");
+  const boxSelection = document.querySelectorAll(".gameSquare");
+  const resetButton = document.querySelector("#reset");
+  resetButton.addEventListener("click", () => {
+    gameBoard.player1.currentPlayer = true;
+    gameBoard.player2.currentPlayer = false;
+    gameBoard.board = ["", "", "", "", "", "", "", "", ""];
+    gameText.textContent = gameBoard.player1.name + "'s turn";
+    Play();
+    renderGame();
+  });
   let boxArray = [...boxSelection];
 
-  gameBoard = {
-    currentPlayer: "X",
+  function startPage() {
+    const startButton = document.querySelector("#start");
+    const player1Input = document.querySelector("#player1");
+    const player2Input = document.querySelector("#player2");
+    const startingPage = document.querySelector("#startingPage");
+    const mainGame = document.querySelector("#mainGame");
+
+    startButton.addEventListener("click", (e) => {
+      if (player1Input.value != "" && player2Input.value != "") {
+        gameBoard.player1.name = player1Input.value;
+        gameBoard.player2.name = player2Input.value;
+        gameText.textContent = gameBoard.player1.name + "'s turn";
+        startingPage.setAttribute("class", "removeDisplay");
+        mainGame.removeAttribute("class", "removeDisplay");
+        Play();
+      }
+    });
+  }
+  startPage();
+
+  let gameBoard = {
+    player1: { name: "player 1", value: "X", currentPlayer: true },
+    player2: { name: "player 2", value: "O", currentPlayer: false },
     board: ["", "", "", "", "", "", "", "", ""],
   };
-
   function renderGame() {
     boxArray.forEach((e) => {
       e.textContent = gameBoard.board[boxArray.indexOf(e)];
@@ -21,6 +50,7 @@ const ticTacToe = (function () {
   function checkWinner() {
     if (gameBoard.board.every(checkForDraw)) {
       gameText.textContent = "DRAW";
+      endGame();
     } else {
     }
     //logic for X
@@ -53,7 +83,8 @@ const ticTacToe = (function () {
         gameBoard.board[4] === "X" &&
         gameBoard.board[6] === "X")
     ) {
-      gameText.textContent = "X WON";
+      gameText.textContent = gameBoard.player1.name + " WON";
+      endGame();
     }
     //logic for O
     if (
@@ -85,27 +116,42 @@ const ticTacToe = (function () {
         gameBoard.board[4] === "O" &&
         gameBoard.board[6] === "O")
     ) {
-      gameText.textContent = "O WON";
+      gameText.textContent = gameBoard.player2.name + " WON";
+      endGame();
     }
   }
+  function endGame() {
+    boxArray.forEach((element) => {
+      element.removeEventListener("click", currentPlayer);
+    });
+  }
+  function currentPlayer() {
+    if (gameBoard.board[boxArray.indexOf(this)] === "") {
+      if (gameBoard.player1.currentPlayer === true) {
+        gameBoard.board[boxArray.indexOf(this)] = gameBoard.player1.value;
+        gameBoard.player1.currentPlayer = false;
+        gameBoard.player2.currentPlayer = true;
+        gameText.textContent = gameBoard.player2.name + "'s turn";
+      } else {
+        gameBoard.board[boxArray.indexOf(this)] = gameBoard.player2.value;
+        gameBoard.player1.currentPlayer = true;
+        gameBoard.player2.currentPlayer = false;
+        gameText.textContent = gameBoard.player1.name + "'s turn";
+      }
+    }
+    renderGame();
+    checkWinner();
+  }
+
+  function boxEventListeners() {
+    boxArray.forEach((element) => {
+      element.addEventListener("click", currentPlayer);
+    });
+  }
+  function Play() {
+    boxEventListeners();
+  }
   return {
-    Play() {
-      boxArray.forEach((element) => {
-        element.addEventListener("click", () => {
-          if (gameBoard.board[boxArray.indexOf(element)] === "") {
-            gameBoard.board[boxArray.indexOf(element)] =
-              gameBoard.currentPlayer;
-            renderGame();
-            checkWinner();
-            if (gameBoard.currentPlayer === "X") {
-              gameBoard.currentPlayer = "O";
-            } else {
-              gameBoard.currentPlayer = "X";
-            }
-          }
-        });
-      });
-    },
+    gameBoard: gameBoard,
   };
 })();
-ticTacToe.Play();
